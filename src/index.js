764187de0a6350d83b2161cc30fb08726227bcc4
agrimbansal401 <agrimbansal9894@gmail.com>
2026-02-14 19:31:59 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
 import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-function ProductCard({ name, price, inStock }) {
-  return (
-    <div className="card">
-      <h3>{name}</h3>
-      <p className="price">₹{price}</p>
-      <p className={inStock ? "in-stock" : "out-stock"}>
-        {inStock ? "In Stock" : "Out of Stock"}
-      </p>
-    </div>
-  );
-}
-
-export default ProductCard;
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
