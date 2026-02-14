79aadb6683ba85a05c0d0e0224717ae8bed18668
agrimbansal401 <agrimbansal9894@gmail.com>
2026-02-14 19:31:26 +0530

    Update productcard.js

--- src/productcard.js
+++ src/productcard.js
@@ -1,1 +1,15 @@
+import React from "react";
 
+function ProductCard({ name, price, inStock }) {
+  return (
+    <div className="card">
+      <h3>{name}</h3>
+      <p className="price">₹{price}</p>
+      <p className={inStock ? "in-stock" : "out-stock"}>
+        {inStock ? "In Stock" : "Out of Stock"}
+      </p>
+    </div>
+  );
+}
+
+export default ProductCard;
